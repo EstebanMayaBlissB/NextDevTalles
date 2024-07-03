4abea87e1f19c773ca504f8d7b77296c1d86ff83
EstebanMa12 <desteban.maya@udea.edu.co>
2024-07-02 19:48:19 -0500

    First Commit

---
+++ app/(general)/contact/page.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "Contact Page",
+  description: "Contact Page",
+  keywords: ["contact", "page", "contact page"],
+};
+
+function ContactPage() {
+  return <div>ContactPage</div>;
+}
+
+export default ContactPage;
